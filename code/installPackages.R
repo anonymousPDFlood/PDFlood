9a8907ad3688d577666c2c9f6dfbbfc487a21f7e
anonymousPDFlood <samantha.m.roth@dartmouth.edu>
2025-11-14 09:17:28 -0500

    Create installPackages.R

---
+++ code/installPackages.R
@@ -0,0 +1,16 @@
+#install necessary R packages
+
+rm(list=ls())
+
+install.packages("terra", type = "source")
+install.packages("akima")
+install.packages("DescTools")
+install.packages("MASS")
+install.packages("Matrix")
+install.packages("fields")
+install.packages("mvtnorm")
+install.packages("spam")
+install.packages("RColorBrewer")
+install.packages("ggplot2")
+install.packages("ggmap")
+install.packages("osmdata")
